89cff0439dd9367317b4213b070d79afae14a37a
Karyn Fogg <karyn.fogg@theprimacy.com>
2021-07-22 16:30:25 -0400

    fix app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import AppRouter from "./routers/AppRouter";
-import configureStore from "./store/configureStore";
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -17,5 +18,8 @@ const jsx = (
     </Provider>
 );
 
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-ReactDOM.render(jsx, document.getElementById('app'));
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
